bdd4eae01922caf7059fe9d584ac15c64f8147f4
Andreas Krennmair <ak@synflood.at>
2011-07-01 23:31:04 +0200

    optionally log to syslog.

--- log.go
+++ log.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"syslog"
+	"log"
 )
 
 type LogLevel int
@@ -22,17 +24,55 @@ type LogMsg struct {
 	Timestamp *time.Time
 }
 
+type SysLogger struct {
+	debug *log.Logger
+	info *log.Logger
+	warn *log.Logger
+	error *log.Logger
+}
+
+func NewSysLogger() *SysLogger {
+	sl := new(SysLogger)
+	sl.debug = syslog.NewLogger(syslog.LOG_DEBUG, log.LstdFlags)
+	sl.info = syslog.NewLogger(syslog.LOG_INFO, log.LstdFlags)
+	sl.warn = syslog.NewLogger(syslog.LOG_WARNING, log.LstdFlags)
+	sl.error = syslog.NewLogger(syslog.LOG_ERR, log.LstdFlags)
+	return sl
+}
+
+func (sl *SysLogger) Log(level LogLevel, msg string) {
+	switch level {
+		case DEBUG:
+			sl.debug.Print(msg)
+		case INFO:
+			sl.info.Print(msg)
+		case WARN:
+			sl.warn.Print(msg)
+		case ERROR:
+			sl.error.Print(msg)
+	}
+}
+
 func NewLogMsg(level LogLevel, msg string) LogMsg {
 	return LogMsg{ level, msg, time.UTC() }
 }
 
 func Logger(c* conf.ConfigFile, logchan chan LogMsg) {
-	// TODO: read configuration about logging threshold, log target, etc.
+	use_syslog := false
+	logmethod, err := c.GetString("", "log_method")
+	if err == nil && logmethod == "syslog" {
+		use_syslog = true
+	}
+	sl := NewSysLogger()
 	for {
 		lm := <-logchan
-		severity := LogLevelToString(lm.Level)
 		timestamp := lm.Timestamp.String()
-		fmt.Fprintf(os.Stderr, "[%s] %s: %s\n", timestamp, severity, lm.Msg)
+		if use_syslog {
+			sl.Log(lm.Level, lm.Msg)
+		} else {
+			severity := LogLevelToString(lm.Level)
+			fmt.Fprintf(os.Stderr, "[%s] %s: %s\n", timestamp, severity, lm.Msg)
+		}
 	}
 }
 
