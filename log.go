d8af73070ed67bb593faed7d73e54f8d6ea42470
Andreas Krennmair <ak@synflood.at>
2013-02-28 22:53:03 +0100

    simply logging code.

--- log.go
+++ log.go
@@ -25,32 +25,26 @@ type LogMsg struct {
 }
 
 type SysLogger struct {
-	debug *log.Logger
-	info  *log.Logger
-	warn  *log.Logger
-	error *log.Logger
+	loggers map[LogLevel]*log.Logger
 }
 
 func NewSysLogger() *SysLogger {
-	sl := &SysLogger{}
-	sl.debug, _ = syslog.NewLogger(syslog.LOG_DEBUG, log.LstdFlags)
-	sl.info, _ = syslog.NewLogger(syslog.LOG_INFO, log.LstdFlags)
-	sl.warn, _ = syslog.NewLogger(syslog.LOG_WARNING, log.LstdFlags)
-	sl.error, _ = syslog.NewLogger(syslog.LOG_ERR, log.LstdFlags)
+	sl := &SysLogger{
+		loggers: make(map[LogLevel]*log.Logger),
+	}
+	sl.loggers[DEBUG],  _ = syslog.NewLogger(syslog.LOG_DEBUG, log.LstdFlags)
+	sl.loggers[INFO], _ = syslog.NewLogger(syslog.LOG_INFO, log.LstdFlags)
+	sl.loggers[WARN], _ = syslog.NewLogger(syslog.LOG_WARNING, log.LstdFlags)
+	sl.loggers[ERROR], _ = syslog.NewLogger(syslog.LOG_ERR, log.LstdFlags)
 	return sl
 }
 
 func (sl *SysLogger) Log(level LogLevel, msg string) {
-	switch level {
-	case DEBUG:
-		sl.debug.Print(msg)
-	case INFO:
-		sl.info.Print(msg)
-	case WARN:
-		sl.warn.Print(msg)
-	case ERROR:
-		sl.error.Print(msg)
+	logger := sl.loggers[level]
+	if logger == nil {
+		panic("invalid log level")
 	}
+	logger.Print(msg)
 }
 
 func NewLogMsg(level LogLevel, msg string) LogMsg {
